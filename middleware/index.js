87b4fc3dfaf024e6b871c2fc434e02bfbeafe6ff
20170228-211617\Administrator <dakani@outlook.com>
2018-10-21 17:55:20 +0800

    添加错误处理

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,6 @@
 const miSend = require("./mi-send/index.js")
+const miHttpError = require("./mi-http-error/index.js")
 module.exports = (app) => {
     app.use(miSend)
+    app.use(miHttpError)
 }
---
+++ middleware/mi-http-error/index.js
@@ -0,0 +1,24 @@
+/**
+ * 错误处理中间件
+ */
+module.exports = async (ctx, next) => {
+	try {
+		await next();
+		/**
+		 * 如果没有更改过 response 的 status，则 koa 默认的 status 是 404 
+		 */
+		if (ctx.response.status === 404 && !ctx.response.body) ctx.throw(404);
+	} catch (e) {
+		let fileName = 'other' 
+		/*此处进行错误处理，下面会讲解具体实现*/
+		const status = e.status;
+		switch(status) {
+			case 400:;
+			case 404:;
+			case 500: fileName = status; break;
+			default: fileName = 'other'; break;
+		}
+		// 接下来针对各个不同的状态做出对应的处理
+		console.log(fileName)
+	}
+}
